c1cabb960a79d85c947208f5a1a93856213f18c0
Kaavya <kaavya.bar@gmail.com>
2023-03-05 01:22:29 +0530

    color difference navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import "./index.css";
 import Events from './routes/Events/Events';
+import Home from './routes/Home/Home';
 import Timeline from './routes/Timeline/Timeline';
 // import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <Home/>
       <Events />
       <Timeline />
     </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,23 @@
 import React from 'react'
-import { Link, NavLink} from "react-router-dom";
+import "./Navbar.scss"
+// import { Link, NavLink} from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="navbar_left">
+        JSCOP 5.0
+      </div>
+      <div className="navbar_right">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">Events</a></li>
+          <li><a href="/">Organiser</a></li>
+          <li><a href="/">Passes</a></li>
+
+        </ul>
+      </div>
+    </div>
   )
 }
 
