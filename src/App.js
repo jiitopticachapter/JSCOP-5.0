f025e534ea82a618fbc19f656ffb37958f1a2b61
Harsh Sharma <harshsharma20503@gmail.com>
2023-03-09 21:10:31 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import React from "react"
+
+import Register from './Pages/Register/Register';
+import EventDetails from './Pages/EventDetails/EventDetails';
+import Home from './Pages/Home/Home';
+import PageNotFound from './Pages/PageNotFound/PageNotFound'
 import Navbar from './Components/Navbar/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
-}
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
+function App() {
+    return (
+      <BrowserRouter>
+        <Navbar/>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/register' element={<Register/>}></Route>
+            <Route path='/eventdetails' element={<EventDetails/>}></Route>
+            <Route path='*' element={<PageNotFound/>}></Route>
+          </Routes>
+      </BrowserRouter>
+    );
+}
+  
 export default App;
