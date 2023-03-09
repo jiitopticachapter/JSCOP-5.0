014a01472fb08be4b445ec2bc680802c38f15eaf
Kaavya <kaavya.bar@gmail.com>
2023-03-09 16:09:14 +0530

    landing page update

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,6 +6,11 @@ const LandingPage = () => {
   return (
     <div className="landing">
       <Navbar />
+
+      <div className="landing_content">
+        <div className="landing_heading">JSCOP <span>5.0</span></div>
+        <div className="landing_subhead">JIIT Student Conference on Optics and Photonics</div>
+      </div>
     </div>
   )
 }
