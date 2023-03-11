49da1afc81e4605e59b96147fcea50b58af6756f
Nancy <nancysoni004@gmail.com>
2023-03-12 00:28:14 +0530

    Page Not Found Created

--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-
+import "./PageNotFound.scss"
 const PageNotFound = () => {
     return (
       <motion.div initial = {{opacity:0}} animate = {{opacity:1}} exit = {{opacity: 0}} transition = {{duration: 2}} 
       className='' 
       >
-          PageNotFound
+          <div id="background"></div>
+<div className="top">
+  <h1>404</h1>
+  <h3>page not found</h3>
+</div>
+
+<div className="bottom">
+  
+  
+  <div className="buttons">
+    <button className="btn">Back</button>
+    <button className="btn">Home</button>
+  </div>
+</div>
+
+
       </motion.div>
     )
 }
