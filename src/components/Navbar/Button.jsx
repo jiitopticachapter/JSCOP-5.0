624cdeaca4f5b0bf7644e784973ec56a62acf54b
Kaavya <kaavya.bar@gmail.com>
2023-03-12 00:12:42 +0530

    scroll removed from nav items

--- src/components/Navbar/Button.jsx
+++ src/components/Navbar/Button.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import './Button.css';
+import './Button.scss';
 import { Link } from 'react-router-dom';
 
 const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
