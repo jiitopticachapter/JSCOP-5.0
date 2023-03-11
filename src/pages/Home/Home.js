00170193700263f158ed8d8aaebb413320fc2403
Kaavya <kaavya.bar@gmail.com>
2023-03-11 16:58:09 +0530

    working navbar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import LandingPage from '../../components/LandingPage/LandingPage'
-import Quote from '../../components/Quote/Quote'
+// import LandingPage from '../../components/LandingPage/LandingPage'
+// import Quote from '../../components/Quote/Quote'
 import Events from '../../components/Events/Events'
 import Timeline from '../../components/Timeline/Timeline'
 import Speakers from '../../components/Speakers/Speakers'
 import Sponsors from '../../components/Sponsors/Sponsors'
+// import Navbar from '../../components/Navbar/Navbar'
 
 const Home = () => {
     return (
       <motion.div initial = {{opacity:0}} animate = {{opacity:1}} exit = {{opacity: 0}} transition = {{duration: 2}}
         className='' 
       >
-        <LandingPage/>
-        <Quote/>
+      
+        {/* <Quote/> */}
         <Events/>
         <Timeline/>
         <Speakers/>
