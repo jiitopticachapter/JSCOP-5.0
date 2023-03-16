ff918f66ba2efc00a1c08fff3a59169734784899
Kaavya <kaavya.bar@gmail.com>
2023-03-17 01:46:19 +0530

    home page update

--- src/components/FAQ/faq.js
+++ src/components/FAQ/faq.js
@@ -9,7 +9,7 @@ export default function Faq() {
       question: "How many programmers does it take to screw a lightbulb?",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
-      open: true
+      open: false
     },
     {
       question: "Who is the most awesome person?",
