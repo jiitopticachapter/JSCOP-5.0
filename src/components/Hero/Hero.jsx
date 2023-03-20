82a66d16ea5b687665a5e04959c1e9886cddff00
Kaavya <kaavya.bar@gmail.com>
2023-03-20 16:30:17 +0530

    video playback rate
    - new gif background added in heading

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import {React, useRef} from 'react'
 import "./Hero.scss"
 import clip from './vid-bg1.mp4';
 import Poster from './bg5.jpg';
 
 const Hero = () => {
-  return (
-    <div className="hero-container">
-      {/* <video src="/assets/videos/vid-bg2.mp4" autoPlay loop muted /> */}
+  const videoRef = useRef();
+  const setPlayBack = () => {
+    videoRef.current.playbackRate = 0.6;
+  }
+    return (
+      <div className="hero-container">
+        {/* <video src="/assets/videos/vid-bg2.mp4" autoPlay loop muted /> */}
 
 
-      <video autoPlay loop muted poster={Poster}>
-        <source src={clip} type='video/mp4' />
-        <source src={clip} type="video/ogg" />
-      </video>
-    
+        <video autoPlay loop muted poster={Poster}
+          ref={videoRef}
+          onCanPlay={() => setPlayBack()}>
+          <source src={clip} type='video/mp4' />
+          <source src={clip} type="video/ogg" />
+        </video>
+
         <h1>JSCOP <span>5.0</span></h1>
         <p>JIIT Student conference for optics and photonics</p>
-     
-    </div>
-  )
-}
 
-export default Hero
+      </div>
+    )
+  }
+
+  export default Hero
