6800397978da3509c8e8aa9439a37047cdf0b144
namangupta28 <namangupta990@gmail.com>
2023-03-31 02:11:26 +0530

    fixed devfolio issue

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -9,50 +9,21 @@ const Sponsors = () => {
         <div className="Sponsors_slider">
         <div className="Sponsors_slide-track">
         <div className="Sponsors_slide">
-            <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/replit_logo_icon_168861.png" height="100" width="250" alt="" />
+            <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/replit_logo_icon_168861.png" height="100" width="250" alt="REPLIT LOGO" />
           </div>
           <div className="Sponsors_slide">
-            <img src="https://www.pngall.com/wp-content/uploads/10/Filecoin-Crypto-Logo-PNG-File.png" width="250" alt="" />
+            <img src="https://www.pngall.com/wp-content/uploads/10/Filecoin-Crypto-Logo-PNG-File.png" width="250" alt="FILECOIN LOGO" />
           </div>
           <div className="Sponsors_slide">
-          <img src="https://jithack.tech/images/logo/Devfolio_Logo-White@2x.png" width="250" alt="" />
+          <img src="https://jithack.tech/images/logo/Devfolio_Logo-White@2x.png" width="250" alt="DEVFOLIO LOGO" />
           </div>
           <div className="Sponsors_slide">
-            <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/replit_logo_icon_168861.png" height="100" width="250" alt="" />
+            <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/replit_logo_icon_168861.png" height="100" width="250" alt="SOLANA LOGO" />
           </div>
           <div className="Sponsors_slide">
-            <img src="https://www.pngall.com/wp-content/uploads/10/Filecoin-Crypto-Logo-PNG-File.png" width="250" alt="" />
+            <img src="https://www.pngall.com/wp-content/uploads/10/Filecoin-Crypto-Logo-PNG-File.png" width="250" alt="POLYGON LOGO" />
           </div>
-          <div className="Sponsors_slide">
-          <img src="https://jithack.tech/images/logo/Devfolio_Logo-White@2x.png" width="250" alt="" />
-          </div>
-          <div className="Sponsors_slide">
-            <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/replit_logo_icon_168861.png" height="100" width="250" alt="" />
-          </div>
-          <div className="Sponsors_slide">
-            <img src="https://www.pngall.com/wp-content/uploads/10/Filecoin-Crypto-Logo-PNG-File.png" width="250" alt="" />
-          </div>
-          <div className="Sponsors_slide">
-          <img src="https://jithack.tech/images/logo/Devfolio_Logo-White@2x.png" width="250" alt="" />
-          </div>
-          <div className="Sponsors_slide">
-            <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/replit_logo_icon_168861.png" height="100" width="250" alt="" />
-          </div>
-          <div className="Sponsors_slide">
-            <img src="https://www.pngall.com/wp-content/uploads/10/Filecoin-Crypto-Logo-PNG-File.png" width="250" alt="" />
-          </div>
-          <div className="Sponsors_slide">
-          <img src="https://jithack.tech/images/logo/Devfolio_Logo-White@2x.png" width="250" alt="" />
-          </div>
-          <div className="Sponsors_slide">
-            <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/replit_logo_icon_168861.png" height="100" width="250" alt="" />
-          </div>
-          <div className="Sponsors_slide">
-            <img src="https://www.pngall.com/wp-content/uploads/10/Filecoin-Crypto-Logo-PNG-File.png" width="250" alt="" />
-          </div>
-          <div className="Sponsors_slide">
-          <img src="https://jithack.tech/images/logo/Devfolio_Logo-White@2x.png" width="250" alt="" />
-          </div>
+          
           
         </div>
       </div>
