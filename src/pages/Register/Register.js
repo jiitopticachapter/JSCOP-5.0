db0804ef68be155ef9d283adae9eeccbc2c43e38
unknown <aryanarora5165@gmail.com>
2023-04-11 20:16:51 +0530

    updated register page

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -5,58 +5,58 @@ import PageName from '../../components/PageName/PageName'
 import {useState} from 'react'
 import registerImg from '../../assets/images/1.png'
 import axios from 'axios'
-// import ComingSoon from '../../components/ComingSoon/ComingSoon'
+import ComingSoon from '../../components/ComingSoon/ComingSoon'
 // import $ from "jquery";
 import "jquery-ui-dist/jquery-ui";
 
 const Register = () => {
-   const [data,setData]=useState({
-      username:"",
-      email:"",
-      enrol:"",
-      branch:"",
-      year:"",
-      mobile:""
-   })
+//    const [data,setData]=useState({
+//       username:"",
+//       email:"",
+//       enrol:"",
+//       branch:"",
+//       year:"",
+//       mobile:""
+//    })
 
-   const [uploaded,setUploaded] = useState(false);
-   const [filename,setFilename] = useState("PAYMENT SCREENSHOT");
-   const [file,setFile] = useState(null);
+//    // const [uploaded,setUploaded] = useState(false);
+//    const [filename,setFilename] = useState("PAYMENT SCREENSHOT");
+//    const [file,setFile] = useState(null);
 
-   const handleFileChange = (e) =>{
-      setFile(e.target.files[0]);
-      setUploaded(true);
-      console.log(e.target.files[0]);
-      setFilename(e.target.files[0].name);
-   }
+//    const handleFileChange = (e) =>{
+//       setFile(e.target.files[0]);
+//       setUploaded(true);
+//       console.log(e.target.files[0]);
+//       setFilename(e.target.files[0].name);
+//    }
 
-   const handleChange=e=>{
-      const {name,value}=e.target;
-      setData({
-         ...data,
-         [name]:value
-      })
-      console.log(e);
-   }
+//    const handleChange=e=>{
+//       const {name,value}=e.target;
+//       setData({
+//          ...data,
+//          [name]:value
+//       })
+//       console.log(e);
+//    }
 
-   const postData=async(e)=>{
-      console.log(data)
-      e.prevenDefault();
-      if(data && file)
-      {
-         // react toastify notification of success
-      }
+//    const postData=async(e)=>{
+//       console.log(data)
+//       e.prevenDefault();
+//       if(data && file)
+//       {
+//          // react toastify notification of success
+//       }
 
-      try{
+//       try{
 
-        await axios.post("http://localhost:5001/user/register",{
-            data
-         }).then(res=>console.log(res));
-      }
-      catch(err){console.log("Eror in sending daa from f->b")
+//         await axios.post("http://localhost:5001/user/register",{
+//             data
+//          }).then(res=>console.log(res));
+//       }
+//       catch(err){console.log("Eror in sending daa from f->b")
 
-      }
-   }
+//       }
+//    }
 
    
 // const [uploaded,setUploaded] = useState(false);
@@ -65,7 +65,7 @@ const Register = () => {
       <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 2 }}
          className='register-page'>
          <PageName page_heading="REGISTER" visible="none"/>
-         <div className='register-form-container'>
+         {/* <div className='register-form-container'>
             <form method="post">
                <h1 className='register-heading'>Registration Form</h1>
                <fieldset>
@@ -75,9 +75,16 @@ const Register = () => {
                   <input type="email" name="email" id="email" value={data.email} onChange={handleChange} placeholder="E-MAIL" required />
                   <br />
                   <input type="number" name="enrol" id="enroll" value={data.enrol} onChange={handleChange} placeholder="ENROLLMENT NUMBER" required />
-                  <br />
-                  <input type="text" name="branch" id="branch" value={data.branch} onChange={handleChange} placeholder="BRANCH" required />
-                  <br />
+                  <br /> */} */
+                  {/* <input type="text" name="branch" id="branch" value={data.branch} onChange={handleChange} placeholder="BRANCH" required /> */}
+                  {/* <select name="branch" id="branch" value={data.branch} onChange={handleChange} required >
+                     <option className="branch_dropdown" value="" disabled selected hidden>SELECT YOUR BRANCH</option>
+                     <option  className="branch_dropdown" value="CSE">CSE</option>
+                     <option  className="branch_dropdown" value="IT">IT</option>
+                     <option  className="branch_dropdown" value="ECE">ECE</option>
+                     <option  className="branch_dropdown" value="BIOTECHNOLOGY">BIOTECHNOLOGY</option>
+                  </select> */}
+                  {/* <br />
                   <input type="text" name="year" id="year" value={data.year} onChange={handleChange} placeholder="YEAR" required />
                   <br />
                   <input type="number" name="mobile" id="Mob" value={data.mobile} onChange={handleChange} placeholder="MOBILE NUMBER" required />
@@ -89,12 +96,12 @@ const Register = () => {
                   <label htmlFor="submit"></label>
                   <input type="submit" name="submit" id="submit" onClick={postData} value="REGISTER" />
                </fieldset>
-            </form>
-            <div className="Register-Image">
+            </form> */}
+            {/* <div className="Register-Image">
                <img src={registerImg} alt=' ' />
-            </div>
-         </div>
-         {/* <ComingSoon/> */}
+            </div> */}
+         {/* </div> */}
+         <ComingSoon/>
       </motion.div>
    )
 }
