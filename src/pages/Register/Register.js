357ffa88584d764bb10a511cd1a99675a2c0e34b
Kaavya <kaavya.bar@gmail.com>
2023-04-30 01:54:46 +0530

    registrations closed

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,243 +1,276 @@
-import React, { useEffect } from "react";
-import { motion } from "framer-motion";
-import "./Register.scss";
-import PageName from "../../components/PageName/PageName";
-import { useState } from "react";
-import registerImg from "../../assets/images/1.png";
-import "jquery-ui-dist/jquery-ui";
-import { storage } from "../../firebase";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import axios from "axios";
-// import { upload } from '@testing-library/user-event/dist/upload'
-// import ComingSoon from '../../components/ComingSoon/ComingSoon'
+// import React, { useEffect } from "react";
+// import { motion } from "framer-motion";
+// import "./Register.scss";
+// import PageName from "../../components/PageName/PageName";
+// import { useState } from "react";
+// import registerImg from "../../assets/images/1.png";
+// import "jquery-ui-dist/jquery-ui";
+// import { storage } from "../../firebase";
+// import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+// import axios from "axios";
+// // import { upload } from '@testing-library/user-event/dist/upload'
+// // import ComingSoon from '../../components/ComingSoon/ComingSoon'
 
-const regex2 = /^[\w-.]+@([\w-]+\.)+[\w-]{2,}$/;
-const regex3 = /^\d{10}$/;
+// const regex2 = /^[\w-.]+@([\w-]+\.)+[\w-]{2,}$/;
+// const regex3 = /^\d{10}$/;
+
+// const Register = () => {
+//   const [uploaded, setUploaded] = useState(false);
+//   const [filename, setFilename] = useState("PAYMENT SCREENSHOT");
+//   const [file, setFile] = useState(null);
+//   const [imgurl, setImgurl] = useState(""); //url of payment screenshot
+
+//   const [data, setData] = useState({
+//     username: "",
+//     email: "",
+//     enrol: "",
+//     branch: "CSE",
+//     year: "",
+//     mobile: "",
+//     setNew: "YES",
+//     Verified: "NO",
+//   });
+//   const handleFileChange = (e) => {
+//     setFile(e.target.files[0]);
+//     setUploaded(true);
+//     console.log(e.target.files[0]);
+//     setFilename(e.target.files[0].name);
+//   };
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setData({
+//       ...data,
+//       [name]: value,
+//     });
+//     console.log(data);
+//   };
+
+//   useEffect(() => {
+//     console.log(imgurl);
+//     console.log(data);
+//   }, [imgurl, data]);
+
+//   const handlesubmit = (event) => {
+//     console.log(data);
+//     event.preventDefault();
+//     if (data && file) {
+//       if(!regex2.test(data.email))
+//       {
+//          return alert("Invalid Email")
+//       }
+//       if(!regex3.test(data.mobile))
+//       {
+//          return alert("Invalid mobile number");
+//       }
+
+//       const storageRef = ref(storage, `${data.enrol + "/"}` + file.name);
+//       const uploadTask = uploadBytesResumable(storageRef, file);
+//       uploadTask.on(
+//         "state_changed",
+//         (snapshot) => {},
+//         (error) => {
+//           console.log(error);
+//         },
+//         () => {
+//           getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+//             alert("Screenshot uploaded successfully");
+//             setImgurl(downloadUrl);
+//           });
+//         }
+//       );
+
+//       const Form = {
+//         username: data.username,
+//         email: data.email,
+//         enrol: data.enrol,
+//         branch: data.branch,
+//         year: data.year,
+//         mobile: data.mobile,
+//         Paid: imgurl,
+//       };
+//       axios
+//         .post("https://backend.jiitopticachapter.in/frontdata", Form)
+//         .then(function () {
+//           alert("Successfully registered");
+//           // window.location.href = "http://localhost:3000/";
+//         });
+//     }
+//   };
+
+//   return (
+//     <motion.div
+//       initial={{ opacity: 0 }}
+//       animate={{ opacity: 1 }}
+//       exit={{ opacity: 0 }}
+//       transition={{ duration: 2 }}
+//       className="register-page"
+//     >
+//       <PageName page_heading="REGISTER" visible="none" />
+//       <div className="register-form-container">
+//         <form>
+//           <h1 className="register-heading">Registration Form</h1>
+//           <fieldset>
+//             <br />
+//             <input
+//               type="text"
+//               name="username"
+//               value={data.username}
+//               onChange={handleChange}
+//               id="username"
+//               placeholder="NAME"
+//               required
+//             />
+//             <br />
+//             <input
+//               type="email"
+//               name="email"
+//               id="email"
+//               value={data.email}
+//               onChange={handleChange}
+//               placeholder="E-MAIL"
+//               required
+//             />
+//             <br />
+//             <input
+//               type="number"
+//               name="enrol"
+//               id="enroll"
+//               value={data.enrol}
+//               onChange={handleChange}
+//               placeholder="ENROLLMENT NUMBER"
+//               required
+//             />
+//             <br />
+//             <select
+//               name="branch"
+//               id="branch"
+//               value={data.branch}
+//               onChange={handleChange}
+//               required
+//             >
+//               {/* <option
+//                 className="branch_dropdown"
+//                 value=""
+//                 selected
+//                 disabled
+//                 hidden
+//               >
+//                 SELECT YOUR BRANCH
+//               </option> */}
+//               <option className="branch_dropdown" value="CSE">
+//                 CSE
+//               </option>
+//               <option className="branch_dropdown" value="IT">
+//                 IT
+//               </option>
+//               <option className="branch_dropdown" value="ECE">
+//                 ECE
+//               </option>
+//               <option className="branch_dropdown" value="BIOTECHNOLOGY">
+//                 BIOTECHNOLOGY
+//               </option>
+//               <option className="branch_dropdown" value="CSE_INT">
+//                 CSE INTEGRATED
+//               </option>
+//               <option className="branch_dropdown" value="ECE_INT">
+//                 ECE INTEGRATED
+//               </option>
+//               <option className="branch_dropdown" value="BBA">
+//                 BBA
+//               </option>
+//               <option className="branch_dropdown" value="OTHER">
+//                 OTHER
+//               </option>
+//             </select>
+//             <br />
+//             <input
+//               type="text"
+//               name="year"
+//               id="year"
+//               value={data.year}
+//               onChange={handleChange}
+//               placeholder="YEAR"
+//               required
+//             />
+//             <br />
+//             <input
+//               type="number"
+//               name="mobile"
+//               id="Mob"
+//               value={data.mobile}
+//               onChange={handleChange}
+//               placeholder="MOBILE NUMBER"
+//               required
+//             />
+//             <br />
+//             <input
+//               type="file"
+//               id="screenshot"
+//               onChange={handleFileChange}
+//               style={{ display: "none" }}
+//               accept="image/*"
+//               required
+//             />
+//             <label className="screenshot-label" htmlFor="screenshot">
+//               <i
+//                 className={`fa-solid ${
+//                   uploaded === true ? "fa-check check-color" : "fa-circle"
+//                 } file-tick`}
+//               ></i>
+//               {filename}
+//             </label>
+//             <br /> <br />
+//             <label htmlFor="submit"></label>
+//             <input
+//               type="submit"
+//               name="submit"
+//               id="submit"
+//               onClick={handlesubmit}
+//               value="REGISTER"
+//             />
+//           </fieldset>
+//         </form>
+//         <div className="Register-Image">
+//           <img src={registerImg} alt=" " />
+//         </div>
+//       </div>
+//       {/* <ComingSoon/> */}
+//     </motion.div>
+//   );
+// };
+
+// export default Register;
+
+import React from 'react'
+import { motion } from 'framer-motion'
+import "./Register.scss"
+import {Link} from 'react-router-dom'
 
 const Register = () => {
-  const [uploaded, setUploaded] = useState(false);
-  const [filename, setFilename] = useState("PAYMENT SCREENSHOT");
-  const [file, setFile] = useState(null);
-  const [imgurl, setImgurl] = useState(""); //url of payment screenshot
-
-  const [data, setData] = useState({
-    username: "",
-    email: "",
-    enrol: "",
-    branch: "CSE",
-    year: "",
-    mobile: "",
-    setNew: "YES",
-    Verified: "NO",
-  });
-  const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
-    setUploaded(true);
-    console.log(e.target.files[0]);
-    setFilename(e.target.files[0].name);
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setData({
-      ...data,
-      [name]: value,
-    });
-    console.log(data);
-  };
-
-  useEffect(() => {
-    console.log(imgurl);
-    console.log(data);
-  }, [imgurl, data]);
-
-  const handlesubmit = (event) => {
-    console.log(data);
-    event.preventDefault();
-    if (data && file) {
-      if(!regex2.test(data.email))
-      {
-         return alert("Invalid Email")
-      }
-      if(!regex3.test(data.mobile))
-      {
-         return alert("Invalid mobile number");
-      }
-
-      const storageRef = ref(storage, `${data.enrol + "/"}` + file.name);
-      const uploadTask = uploadBytesResumable(storageRef, file);
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {},
-        (error) => {
-          console.log(error);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
-            alert("Screenshot uploaded successfully");
-            setImgurl(downloadUrl);
-          });
-        }
-      );
-
-      const Form = {
-        username: data.username,
-        email: data.email,
-        enrol: data.enrol,
-        branch: data.branch,
-        year: data.year,
-        mobile: data.mobile,
-        Paid: imgurl,
-      };
-      axios
-        .post("https://backend.jiitopticachapter.in/frontdata", Form)
-        .then(function () {
-          alert("Successfully registered");
-          // window.location.href = "http://localhost:3000/";
-        });
-    }
-  };
-
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 2 }}
-      className="register-page"
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 2 }}
+      className=''
     >
-      <PageName page_heading="REGISTER" visible="none" />
-      <div className="register-form-container">
-        <form>
-          <h1 className="register-heading">Registration Form</h1>
-          <fieldset>
-            <br />
-            <input
-              type="text"
-              name="username"
-              value={data.username}
-              onChange={handleChange}
-              id="username"
-              placeholder="NAME"
-              required
-            />
-            <br />
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={data.email}
-              onChange={handleChange}
-              placeholder="E-MAIL"
-              required
-            />
-            <br />
-            <input
-              type="number"
-              name="enrol"
-              id="enroll"
-              value={data.enrol}
-              onChange={handleChange}
-              placeholder="ENROLLMENT NUMBER"
-              required
-            />
-            <br />
-            <select
-              name="branch"
-              id="branch"
-              value={data.branch}
-              onChange={handleChange}
-              required
-            >
-              {/* <option
-                className="branch_dropdown"
-                value=""
-                selected
-                disabled
-                hidden
-              >
-                SELECT YOUR BRANCH
-              </option> */}
-              <option className="branch_dropdown" value="CSE">
-                CSE
-              </option>
-              <option className="branch_dropdown" value="IT">
-                IT
-              </option>
-              <option className="branch_dropdown" value="ECE">
-                ECE
-              </option>
-              <option className="branch_dropdown" value="BIOTECHNOLOGY">
-                BIOTECHNOLOGY
-              </option>
-              <option className="branch_dropdown" value="CSE_INT">
-                CSE INTEGRATED
-              </option>
-              <option className="branch_dropdown" value="ECE_INT">
-                ECE INTEGRATED
-              </option>
-              <option className="branch_dropdown" value="BBA">
-                BBA
-              </option>
-              <option className="branch_dropdown" value="OTHER">
-                OTHER
-              </option>
-            </select>
-            <br />
-            <input
-              type="text"
-              name="year"
-              id="year"
-              value={data.year}
-              onChange={handleChange}
-              placeholder="YEAR"
-              required
-            />
-            <br />
-            <input
-              type="number"
-              name="mobile"
-              id="Mob"
-              value={data.mobile}
-              onChange={handleChange}
-              placeholder="MOBILE NUMBER"
-              required
-            />
-            <br />
-            <input
-              type="file"
-              id="screenshot"
-              onChange={handleFileChange}
-              style={{ display: "none" }}
-              accept="image/*"
-              required
-            />
-            <label className="screenshot-label" htmlFor="screenshot">
-              <i
-                className={`fa-solid ${
-                  uploaded === true ? "fa-check check-color" : "fa-circle"
-                } file-tick`}
-              ></i>
-              {filename}
-            </label>
-            <br /> <br />
-            <label htmlFor="submit"></label>
-            <input
-              type="submit"
-              name="submit"
-              id="submit"
-              onClick={handlesubmit}
-              value="REGISTER"
-            />
-          </fieldset>
-        </form>
-        <div className="Register-Image">
-          <img src={registerImg} alt=" " />
+      <div className="reg_over">
+        <div className="top">
+          <h1>REGISTRATIONS</h1>
+         <h1>CLOSED!</h1>
+        </div>
+
+        <div className="bottom">
+
+
+          <div className="buttons">
+            <Link to="/"><button className="btn">Back To Home</button></Link>
+          
+          </div>
         </div>
       </div>
-      {/* <ComingSoon/> */}
-    </motion.div>
-  );
-};
 
-export default Register;
+
+     </motion.div>
+  )
+}
+
+export default Register
